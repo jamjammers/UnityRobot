0ce4f378ac286c42f8ac22409233e0aaf56ea7aa
jamjammers <jamesetam@gmail.com>
2024-01-26 16:41:40 -0600

    remove unnesseasary
    asdf

--- asdjfh/Assets/Scripts/DriveTrain.cs
+++ asdjfh/Assets/Scripts/DriveTrain.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static MotorClass;
 
 public class DriveTrain : MonoBehaviour
 {
